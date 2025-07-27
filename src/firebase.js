098dfa38fc337f56b8bfeadb828fa62df854958f
Indranil102e <indranilsamanta2003@gmail.com>
2025-07-27 18:32:07 +0530

    Add Firebase configuration to firebase.js and update .gitignore to include Firebase-related files

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,14 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDvx5kDXKKNofX8h-KW3ND1LP4CfvWwy4I",
+  authDomain: "sign-up-2ff75.firebaseapp.com",
+  projectId: "sign-up-2ff75",
+  storageBucket: "sign-up-2ff75.firebasestorage.app",
+  messagingSenderId: "205052365518",
+  appId: "1:205052365518:web:3fac33f28d8b78a7f31e9f",
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
